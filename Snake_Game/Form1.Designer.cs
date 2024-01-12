e4ffbbd08c27a66b2523222b616d3d7a62aeb8ec
feifei <liuqingyuff@outlook.com>
2024-01-12 21:44:13 +0800

    init project.

---
+++ Snake_Game/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Snake_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbScore = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.plFood = new System.Windows.Forms.PictureBox();
+            this.plSnake = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plFood)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plSnake)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.ForeColor = System.Drawing.Color.White;
+            this.lbScore.Location = new System.Drawing.Point(10, 15);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(100, 37);
+            this.lbScore.TabIndex = 2;
+            this.lbScore.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(76, 536);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 437);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // plFood
+            // 
+            this.plFood.Image = ((System.Drawing.Image)(resources.GetObject("plFood.Image")));
+            this.plFood.Location = new System.Drawing.Point(1116, 249);
+            this.plFood.Name = "plFood";
+            this.plFood.Size = new System.Drawing.Size(65, 68);
+            this.plFood.TabIndex = 4;
+            this.plFood.TabStop = false;
+            // 
+            // plSnake
+            // 
+            this.plSnake.BackColor = System.Drawing.Color.DimGray;
+            this.plSnake.Location = new System.Drawing.Point(193, 330);
+            this.plSnake.Name = "plSnake";
+            this.plSnake.Size = new System.Drawing.Size(282, 259);
+            this.plSnake.TabIndex = 5;
+            this.plSnake.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(2050, 1250);
+            this.Controls.Add(this.plSnake);
+            this.Controls.Add(this.plFood);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbScore);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plFood)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plSnake)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbScore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox plFood;
+        private System.Windows.Forms.PictureBox plSnake;
+    }
+}
+
---
+++ Snake_Game/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Snake_Game
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        Random random = new Random();
+        int thisWidth = 800;
+        int thisHight = 599;
+        int score = 0;
+        bool keydown;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 0001; timer.Start();
+            timer.Tick += Timer_Tick;
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+            this.Size = new Size(thisWidth, thisHight);
+            this.lbScore.Text = "0";
+            this.KeyUp += Form1_KeyUp;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            keydown = false;
+        }
+
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            this.lbScore.Text = score.ToString();
+            if (keydown)
+            {
+                this.pictureBox1.Visible = true;
+            }
+            else
+            {
+                this.pictureBox1.Visible = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            keydown = true;
+            if (plSnake.Location.Y + plSnake.ClientSize.Height < this.ClientSize.Height)
+            {
+                if (e.KeyCode == Keys.Down)
+                {
+                    plSnake.Location = new Point(plSnake.Location.X, plSnake.Location.Y + 10);
+                }
+            }
+            if (plSnake.Location.Y > 0)
+            {
+                if (e.KeyCode == Keys.Up)
+                {
+                    plSnake.Location = new Point(plSnake.Location.X, plSnake.Location.Y - 10);
+                }
+            }
+            if (plSnake.Location.X > 0)
+            {
+                if (e.KeyCode == Keys.Left)
+                {
+                    plSnake.Location = new Point(plSnake.Location.X - 10, plSnake.Location.Y );
+                }
+            }
+            if (plSnake.Location.X + plSnake.ClientSize.Width < this.ClientSize.Width)
+            {
+                if (e.KeyCode == Keys.Right)
+                {
+                    plSnake.Location = new Point(plSnake.Location.X + 10, plSnake.Location.Y);
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                this.pictureBox1.Location = new Point(this.plSnake.Location.X - 120, this.plSnake.Location.Y - 50);
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                this.pictureBox1.Location = new Point(this.plSnake.Location.X + 50, this.plSnake.Location.Y - 30);
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                this.pictureBox1.Location = new Point(this.plSnake.Location.X - 50 + 10, this.plSnake.Location.Y+50 );
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                this.pictureBox1.Location = new Point(this.plSnake.Location.X - 50 + 10, this.plSnake.Location.Y - 100);
+            }
+            Rectangle food = new Rectangle(plFood.Location.X, plFood.Location.Y, plFood.ClientSize.Width, plFood.ClientSize.Height);
+            Rectangle snake = new Rectangle(plSnake.Location.X, plSnake.Location.Y, plSnake.ClientSize.Width, plSnake.ClientSize.Height);
+            Rectangle fire = new Rectangle(pictureBox1.Location.X, pictureBox1.Location.Y, pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            if (snake.IntersectsWith(food) || fire.IntersectsWith(food))
+            {
+                int x;
+                int y;
+                score++;
+                x = random.Next(0, this.ClientSize.Width + 1);
+                y = random.Next(0, this.ClientSize.Height + 1);
+
+                this.plFood.Location = new Point(x, y);
+            }
+        }
+    }
+}
